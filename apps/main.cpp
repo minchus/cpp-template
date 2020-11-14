4e1c210d2729fdcffb3d3492113e08e9176980b1
Mingu <73884404+minchus@users.noreply.github.com>
2020-11-14 16:23:33 +0000

    fist commit

---
+++ apps/main.cpp
@@ -0,0 +1,6 @@
+#include "ExampleClass.h"
+
+int main() {
+  ExampleClass::do_something();
+  return 0;
+}
---
+++ include/ExampleClass.h
@@ -0,0 +1,19 @@
+//
+// Created on 08/11/2020.
+//
+
+#ifndef EXAMPLE_HEADER_H
+#define EXAMPLE_HEADER_H
+
+#include <iostream>
+
+class ExampleClass {
+
+ public:
+  static void do_something() {
+    std::cout << "Hello world" << std::endl;
+  }
+
+};
+
+#endif  // EXAMPLE_HEADER_H
